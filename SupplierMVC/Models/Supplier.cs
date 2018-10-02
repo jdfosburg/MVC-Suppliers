9382b29cedea3a7a65f569f81c93efc9c7ab3eb4
anga0801 <43789911+anga0801@users.noreply.github.com>
2018-10-02 15:23:28 -0500

    Update Supplier.cs

--- SupplierMVC/Models/Supplier.cs
+++ SupplierMVC/Models/Supplier.cs
@@ -2,22 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace SupplierMVC.Models
 {
     public class Supplier
     {
+        [Required]
         public int SupplierID { get; set; }
+
+        [Display(Name = "Company Name: ")]
         public string CompanyName { get; set; }
+
+        [Display(Name = "Contact Name: ")]
         public string ContactName { get; set; }
+
+        [Display(Name = "Contact Title: ")]
         public string ContactTitle { get; set; }
+
+        [Display(Name = "Address: ")]
         public string Address { get; set; }
+
+        [Display(Name = "City: ")]
         public string City { get; set; }
+
+        [Display(Name = "Region: ")]
         public string Region { get; set; }
+
+        [Display(Name = "Postal code: ")]
         public string PostalCode { get; set; }
+
+        [Display(Name = "Country: ")]
         public string Country { get; set; }
+
+        [Display(Name = "Phone: ")]
         public string Phone { get; set; }
+
+        [Display(Name = "Fax: ")]
         public string Fax { get; set; }
+
+        [Display(Name = "Home Page: ")]
         public string HomePage { get; set; }
     }
 }
